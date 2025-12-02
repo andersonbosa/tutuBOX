96c3aa18c98b904b5843f98c9497af51b3342117
andersonbosa <andersonbosa0@gmail.com>
2025-12-02 20:35:47 -0300

    tentei

--- VScode Platformio/include/pindefs.h
+++ VScode Platformio/include/pindefs.h
@@ -9,21 +9,25 @@
 #define PINDEFS_H
 
 // Button Pin Definitions
-#define BUTTON_PIN_UP      26
-#define BUTTON_PIN_DOWN    33
-#define BUTTON_PIN_CENTER  32  // Exit
-#define BUTTON_PIN_LEFT    25  // Back
-#define BUTTON_PIN_RIGHT   27  // Select
+#define BUTTON_PIN_UP      39
+#define BUTTON_PIN_DOWN    38
+#define BUTTON_PIN_CENTER  37  // Exit
+#define BUTTON_PIN_LEFT    36  // Back
+#define BUTTON_PIN_RIGHT   35  // Select
 
 // Radio Pins
 #define RADIO_CE_PIN_1      5
-#define RADIO_CSN_PIN_1    17
-#define RADIO_CE_PIN_2     16
-#define RADIO_CSN_PIN_2     4
-#define RADIO_CE_PIN_3     15
-#define RADIO_CSN_PIN_3     2
+#define RADIO_CSN_PIN_1     6
+#define RADIO_CE_PIN_2      7
+#define RADIO_CSN_PIN_2     15
+#define RADIO_CE_PIN_3      16
+#define RADIO_CSN_PIN_3     17
 
 // NeoPixel
-#define NEOPIXEL_PIN       14
+#define NEOPIXEL_PIN        14
+
+// Display
+#define DISPLAY_SDA         12
+#define DISPLAY_SCK         13
 
 #endif // PINDEFS_H
